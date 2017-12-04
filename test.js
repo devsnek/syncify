23db02990210ef1ef6b644bd49f35e962b5ae9c4
Gus Caplan <me@gus.host>
2017-12-04 12:43:24 -0600

    tests

--- test.js
+++ test.js
@@ -1,18 +1,39 @@
 const tap = require('tap');
 const syncify = require('.');
 const util = require('util');
+const fs = require('fs');
 const setTimeoutAsync = util.promisify(setTimeout);
+const readFileAsync = util.promisify(fs.readFile);
 
-tap.plan(3);
+tap.plan(4);
 
 tap.type(syncify, 'function', 'is a function');
 
-const start = Date.now();
-syncify(setTimeoutAsync(1000));
-tap.ok(Date.now() - start >= 1000, 'waits for the promise to resolve');
+{
+  const start = Date.now();
+  syncify(setTimeoutAsync(1000));
+  tap.ok(Date.now() - start >= 1000, 'waits for promise to resolve');
+}
 
+try {
+  const p = Promise.reject(new Error('x'));
+  p.catch(() => {}); // eslint-disable-line no-empty-function
+  syncify(p);
+  tap.fail('did not throw!');
+} catch (err) {
+  tap.pass('got expected error');
+}
+
+/*
 tap.throws(() => syncify(Promise.reject(new Error('this is an error'))),
-  'this is an error', 'throws when the promise rejects');
+  'this is an error', 'throws when promise rejects');
+*/
+
+{
+  const expected = fs.readFileSync('./package.json').toString();
+  const questionable = syncify(readFileAsync('./package.json')).toString();
+  tap.ok(expected === questionable, 'fs test');
+}
 
 tap.end();
 

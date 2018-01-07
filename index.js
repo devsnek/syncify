e8664eecff3de092be50115a8d54a7c704228260
Gus Caplan <me@gus.host>
2018-01-07 03:01:52 -0600

    get rid of tick callback

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const {
-  setTickCallback,
   loop,
   kRejected,
   isPromise,
 } = require('bindings')('syncify');
 
 const noop = () => {}; // eslint-disable-line no-empty-function
-
-setTickCallback(process._tickCallback);
 
 module.exports = (promise) => {
   if (!isPromise(promise))
--- syncify.cc
+++ syncify.cc
@@ -1,48 +1,43 @@
 #include <nan.h>
 #include <uv.h>
 
-Nan::Callback tickCallback;
+using namespace v8;
 
-void SetTickCallback(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  tickCallback.SetFunction(info[0].As<v8::Function>());
-}
-
-void Loop(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+void Loop(const Nan::FunctionCallbackInfo<Value>& info) {
   auto isolate = info.GetIsolate();
   
-  v8::Local<v8::Array> ret = v8::Array::New(isolate, 2);
+  Local<Array> ret = Array::New(isolate, 2);
   info.GetReturnValue().Set(ret);
 
   if (!info[0]->IsPromise()) {
-    ret->Set(0, v8::Integer::New(isolate, v8::Promise::kFulfilled));
+    ret->Set(0, Integer::New(isolate, Promise::kFulfilled));
     ret->Set(1, info[0]);
     return;
   }
   
-  v8::Local<v8::Promise> promise = info[0].As<v8::Promise>();
+  Local<Promise> promise = info[0].As<Promise>();
 
   uv_loop_t* loop = uv_default_loop();
   int state = promise->State();
-  while (state == v8::Promise::kPending) {
+  while (state == Promise::kPending) {
     uv_run(loop, UV_RUN_ONCE);
-    tickCallback.Call(0, 0);
+    isolate->RunMicrotasks();
     state = promise->State();
   }
 
-  ret->Set(0, v8::Integer::New(isolate, state));
+  ret->Set(0, Integer::New(isolate, state));
   ret->Set(1, promise->Result());
 }
 
-void IsPromise(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+void IsPromise(const Nan::FunctionCallbackInfo<Value>& info) {
   info.GetReturnValue().Set(info[0]->IsPromise());
 }
 
-void NanSetProperty(v8::Local<v8::Object> recv, const char* name, v8::Local<v8::Value> value) {
+void NanSetProperty(Local<Object> recv, const char* name, Local<Value> value) {
   recv->Set(Nan::New(name).ToLocalChecked(), value);
 }
 
 void Init(v8::Local<v8::Object> exports, v8::Local<v8::Object> module) {
-  Nan::SetMethod(exports, "setTickCallback", SetTickCallback);
   Nan::SetMethod(exports, "loop", Loop);
   Nan::SetMethod(exports, "isPromise", IsPromise);
   NanSetProperty(exports, "kPending", Nan::New(v8::Promise::kPending));
